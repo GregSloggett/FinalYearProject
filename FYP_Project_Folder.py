117e6e63e3980a3ccae0bd8bb6382a3c3ad3edcf
Greg Sloggett <gregory.sloggett@ucdconnect.ie>
2017-11-22 15:54:11 +0000

    add __pycahce__ for python anywhere

--- FYP_Project_Folder.py
+++ FYP_Project_Folder.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -13,6 +13,9 @@ def homepage():
 # The response page that the user is presented with when they authorize the app for the first time.
 @app.route('/response_url/')
 def response_url():
+    error = request.args.get('error')
+    if error == 'access_denied':
+        return render_template("access_denied.html")
 
     return render_template("response_url.html")
 
@@ -21,6 +24,14 @@ def response_url():
 def summary():
 
     return render_template("summary.html")
+
+
+# If the user has already authorized the application this is the page that will be returned (instead of response_url).
+@app.route('/return_user/')
+def return_user():
+
+    return render_template("return_user.html")
+
 
 if __name__ == '__main__':
     app.run()
