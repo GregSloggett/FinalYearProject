726c621e49962773bb3ce0c36efd86aedbe3c437
Greg Sloggett <gregory.sloggett@ucdconnect.ie>
2017-12-05 10:30:11 +0000

    new commit

--- FYP_Project_Folder.py
+++ FYP_Project_Folder.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-#from stravalib.client import Client
+from stravalib.client import Client
 
 
 app = Flask(__name__)
@@ -8,10 +8,24 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://sql2206541:yS3*wS7%@sql2.freemysqlhosting.net:3306/sql2206541'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+#     username="GregorySloggett",
+#     password="Xavi6legend",
+#     hostname="GregorySloggett.mysql.pythonanywhere-services.com",
+#     databasename="GregorySloggett$access_tokens",
+# )
+# app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+# app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+
 db = SQLAlchemy(app)
 
 
 class AccessTokens(db.Model):
+
+    __tablename__ = "access_tokens"
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20))
     access_token = db.Column(db.String(40))
@@ -20,7 +34,7 @@ class AccessTokens(db.Model):
 MY_ACCESS_TOKEN = 'a6e5a504f806ed79c8a6e25f59da056b440faac5'
 MY_CLIENT_ID = 20518
 MY_CLIENT_SECRET = 'cf516a44b390c99b6777f771be0103314516931e'
-#client = Client()
+client = Client()
 
 # Home page of my application.
 @app.route('/', methods=['GET', 'POST'])
@@ -39,13 +53,13 @@ def response_url():
         return render_template("access_denied.html")
 
     code = request.args.get('code')
-    #athlete_access_token = client.exchange_code_for_token(client_id=MY_CLIENT_ID,
-     #                                                     client_secret=MY_CLIENT_SECRET, code=code)
+    athlete_access_token = client.exchange_code_for_token(client_id=MY_CLIENT_ID,
+                                                   client_secret=MY_CLIENT_SECRET, code=code)
 
-    athlete_access_token = 'cc1a2bde123b3868d588fdee5ddec8f1da595903'  ##DELETE THIS LINE TO REMOVE IAN M ACCESS
+#    athlete_access_token = 'cc1a2bde123b3868d588fdee5ddec8f1da595903'  ##DELETE THIS LINE TO REMOVE IAN M ACCESS
 
-    #client.access_token = athlete_access_token
-    #athlete = client.get_athlete()
+    client.access_token = athlete_access_token
+    athlete = client.get_athlete()
 
     check_code = athlete_access_token
     check_access_token = AccessTokens.query.filter_by(access_token=check_code).first()
@@ -57,7 +71,7 @@ def response_url():
     else:
         print('user exists already')
 
-    return render_template("response_url.html")
+    return render_template("response_url.html", athlete_access_token=athlete_access_token)
 
 
 @app.route('/summary/')
