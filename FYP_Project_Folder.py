8ecf58d2a6b1a359df082860e687f9ba7b70ee80
Greg Sloggett <gregory.sloggett@ucdconnect.ie>
2018-02-06 10:29:26 +0000

    Fixed Database issue where user did not have a username. Now uses athlete id.

--- FYP_Project_Folder.py
+++ FYP_Project_Folder.py
@@ -44,8 +44,8 @@ class MyForm(FlaskForm):
 
 class AccessTokens(db.Model):
     __tablename__ = "access_tokens"
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20))
+    # id = db.Column(db.Integer, primary_key=True)
+    athlete_id = db.Column(db.String(11), primary_key=True)
     access_token = db.Column(db.String(40))
 
 
@@ -68,9 +68,9 @@ def homepage():
 
 
 def check_for_cookie():
-    check = request.cookies.get('username')
-    if 'username' in request.cookies:
-        check_access_tokens_database = AccessTokens.query.filter_by(name=check).first()
+    check = request.cookies.get('athlete_id')
+    if 'athlete_id' in request.cookies:
+        check_access_tokens_database = AccessTokens.query.filter_by(athlete_id=check).first()
         if not check_access_tokens_database:
             return False
         else:
@@ -96,30 +96,48 @@ def response_url():
     check_code = athlete_access_token
     check_access_token = AccessTokens.query.filter_by(access_token=check_code).first()
     if not check_access_token:
-        print('user does not exist already')
-        signature = AccessTokens(name=athlete.username, access_token=athlete_access_token)
+        signature = AccessTokens(athlete_id=str(athlete.id), access_token=athlete_access_token)
         db.session.add(signature)
         db.session.commit()
         resp = make_response(render_template("response_url.html", athlete_access_token=athlete_access_token, athlete=athlete,
                                              athlete_stats=client.get_athlete_stats(), athlete_profiler=athlete.profile))
-        resp.set_cookie('username', client.get_athlete().username)
+        resp.set_cookie('athlete_id', str(athlete.id))
         return resp
     else:
         resp = make_response(render_template("return_user.html", athlete_access_token=athlete_access_token, athlete=athlete,
                                              athlete_stats=client.get_athlete_stats(), athlete_profiler=athlete.profile))
-        resp.set_cookie('username', client.get_athlete().username)
+        resp.set_cookie('athlete_id', str(athlete.id))
         return resp
 
 
 @app.route('/summary/', methods=['GET', 'POST'])
 def summary():
-    # accessed_athlete_activities_list = requests.get('https://www.strava.com/api/v3/athlete/activities',
-    #                                                 data={'access_token': client.access_token})
     if check_for_cookie() is False:
         return render_template("homepage.html")
     else:
+        total_rides = 0
+        total_runs = 0
+
+        first_activity = ''
+
+        for activity in client.get_activities(before="2018-02-03T00:00:00Z",
+                                              after="2000-05-10T00:00:00Z", limit=None):
+            first_activity_id = activity.id
+
+            if activity.type == 'Ride':
+                total_rides = total_rides + 1
+            elif activity.type == 'Run':
+                total_runs = total_runs + 1
+
+            first_activity = client.get_activity(activity_id=first_activity_id)
+
+        pie_chart = pygal.Pie()  # Then create a bar graph object
+        pie_chart.add('Rides', total_rides)  # Add some values
+        pie_chart.add('Runs', total_runs)  # Add some values
+        pie_chart = pie_chart.render_data_uri()
         return render_template("summary.html", athlete=client.get_athlete(), athlete_stats=client.get_athlete_stats(),
-                               last_ten_rides=last_ten_rides(), athlete_profiler=client.get_athlete().profile)
+                               last_ten_rides=last_ten_rides(), athlete_profiler=client.get_athlete().profile,
+                               pie_chart=pie_chart, first_activity=first_activity)
 
 
 # If the user has already authorized the application this is the page that will be returned (instead of response_url).
@@ -134,9 +152,6 @@ def return_user():
 
 @app.route('/activity/<activity_id>', methods=['GET', 'POST'])
 def activity(activity_id):
-    # activity_photos = client.get_activity(activity_id=1341714581).full_photos
-    #activity_photos = client.get_activity_photos(activity_id=activity_id, only_instagram=False)
-
     types = ['time', 'latlng', 'altitude', 'heartrate', 'temp', ]
 
     if check_for_cookie() is False:
@@ -153,40 +168,18 @@ def activity(activity_id):
 def map(activity_id, activity_map):
     types = ['time', 'latlng', 'altitude', 'heartrate', 'temp', ]
 
-    # creating a map in the view
-    mymap = Map(
-        identifier="view-side",
-        lat=37.4419,
-        lng=-122.1419,
-        markers=[(37.4419, -122.1419)]
-    )
-    sndmap = Map(
-        identifier="sndmap",
-        lat=37.4419,
-        lng=-122.1419,
-        markers=[
-            {
-                'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
-                'lat': 37.4419,
-                'lng': -122.1419,
-                'infobox': "<b>Hello World</b>"
-            },
-            {
-                'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
-                'lat': 37.4300,
-                'lng': -122.1400,
-                'infobox': "<b>Hello World from other place</b>"
-            }
-        ]
-    )
+    line_chart = pygal.Line()  # Then create a bar graph object
+    line_chart.add('Fibonacci', [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55])  # Add some values
+    line_chart.x_labels = ['0', '5', '10', '15', '20', '25', '30', '35', '40', '42.2']
+    line_chart.y_labels = '0', '2', '4', '6', '8', '10'
+    line_chart = line_chart.render_data_uri()
+
     if check_for_cookie() is False:
         return render_template("homepage.html")
     else:
-        print(client.get_activity(activity_id).start_latitude)
-        print(client.get_activity(activity_id).start_longitude)
         return render_template("map.html", activity_data=client.get_activity(activity_id=activity_id),
-                               mymap=mymap, sndmap=sndmap,
-                               streams=client.get_activity_streams(activity_id=activity_id, types=types, resolution='medium'))
+                               streams=client.get_activity_streams(activity_id=activity_id, types=types, resolution='medium'),
+                               line_chart=line_chart)
 
 
 @app.route('/marathon/')
@@ -258,7 +251,6 @@ def marathon_time_retrieval():
                                        seconds="0", total_minutes=total_minutes, pace=pace, line_chart=line_chart,
                                        kilometres=kilometres_per_minute, bar_chart=bar_chart)
         except ValueError:
-            print("problem")
             this = False
 
 
@@ -278,8 +270,6 @@ def last_ten_rides():
         activity_list.append(activity_id)
 
         # assert len(list(activity_list)) == 10
-
-    print(*activity_list, sep='\n')
 
     return activity_list
 
